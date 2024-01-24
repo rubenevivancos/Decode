d7909f97d4efddaa376073564be588d61ca552bc
rubenevivancos <rubenevivancos@hotmail.com>
2024-01-24 18:22:21 -0500

    Codigo para leer archivo

--- src/decode.js
+++ src/decode.js
@@ -3,3 +3,13 @@ const readline = require('readline');
 
 // Ruta del archivo a leer
 const filePath = 'tu_archivo.txt';
+
+// Objeto para almacenar líneas por índice
+const linesByIndex = {};
+const array = [];
+
+// Crear una interfaz de lectura
+const rl = readline.createInterface({
+    input: fs.createReadStream(filePath),
+    crlfDelay: Infinity // Para manejar correctamente los saltos de línea en Windows
+});
