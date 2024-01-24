0155a4d7a4de5110e37b2685e6c06bd1da54a91d
rubenevivancos <rubenevivancos@hotmail.com>
2024-01-24 18:28:09 -0500

    Ordenando en un arreglo el archivo leido

--- src/decode.js
+++ src/decode.js
@@ -20,3 +20,35 @@ rl.on('line', (line) => {
     linesByIndex[index] = value;
     array.push(index);
 });
+
+// Evento que se activa cuando se completa la lectura del archivo
+rl.on('close', () => {
+    console.log('Valores asignados a índices:', linesByIndex);
+
+    // Ordenar el array de menor a mayor
+    array.sort((a, b) => a - b);
+
+    // Crear un nuevo array con elementos como una piramide de menor a mayor: INICIO
+    const newArray = [];
+    
+    let a = 0; 
+    let b = 1; 
+    let suma = 1;
+    while (b <= array.length) {
+        newArray.push(array.slice(a, b).join(" "));
+        suma++;
+        a = b; 
+        b = b + suma;
+    } 
+    console.log(newArray);
+    // Crear un nuevo array con elementos como una piramide de menor a mayor: FIN
+
+    // Acceder a un valor por su índice
+    const indiceDeseado = 5;
+    if (linesByIndex[indiceDeseado]) {
+        console.log(`Valor en el índice ${indiceDeseado}:`, linesByIndex[indiceDeseado]);
+        console.log("array --> " + array);
+    } else {
+        console.log(`Índice ${indiceDeseado} no encontrado.`);
+    }
+});
